e2c4c979d0122eba6a8c88bfcc48011e8c588236
James Oliver <joliver@rancher.com>
2016-05-23 14:49:20 -0700

    Fixes

--- zookeeper/containers/0.4.0/zookeeper/run.sh
+++ zookeeper/containers/0.4.0/zookeeper/run.sh
@@ -1,12 +1,4 @@
 #!/bin/sh -e
-###############################################################################
-METADATA_HOST=rancher-metadata.rancher.internal
-METADATA_VERSION=2015-12-19
-METADATA=$METADATA_HOST/$METADATA_VERSION
-metadata() { echo $(curl -s $METADATA/$1); }
-get_containers() { echo $(metadata self/service/containers); }
-num_containers() { echo $(get_containers | tr ' ' '\n' | wc -l); }
-###############################################################################
 
 CONFD_INTERVAL=${CONFD_INTERVAL:-60}
 ZK_ENSEMBLE_SIZE=${ZK_ENSEMBLE_SIZE:-1}
@@ -14,12 +6,10 @@ ZK_TICK_TIME=${ZK_TICK_TIME:-2000}
 ZK_INIT_LIMIT=${ZK_INIT_LIMIT:-10}
 ZK_SYNC_LIMIT=${ZK_SYNC_LIMIT:-5}
 ZK_MAX_CLIENT_CXNS=${ZK_MAX_CLIENT_CXNS:-60}
+GIDDYUP_SCALE_TIMEOUT=${GIDDYUP_SCALE_TIMEOUT:-300}
 
 # Wait for all zookeeper nodes to be scheduled
-while [ "$(num_containers)" != "$ZK_ENSEMBLE_SIZE" ]; do
-  echo Found $(num_containers) of $ZK_ENSEMBLE_SIZE nodes
-  sleep $ZK_ENSEMBLE_SIZE
-done
+giddyup service wait scale --timeout $GIDDYUP_SCALE_TIMEOUT
 
 # Write static configuration
 sed -i "s/\$ZK_TICK_TIME/$ZK_TICK_TIME/g" /etc/confd/templates/zoo.cfg.tmpl
