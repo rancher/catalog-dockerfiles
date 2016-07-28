d65c80a31de2b235288a5bb168f786fc672eefa8
James Oliver <joliver@rancher.com>
2016-07-28 11:49:51 -0700

    protect the restore process from external forces

--- etcd/containers/0.10.0/etcd/run.sh
+++ etcd/containers/0.10.0/etcd/run.sh
@@ -144,12 +144,7 @@ disaster_node() {
     echo "Sanitizing DR backup..."
     etcd \
         --name ${NAME} \
-        --listen-client-urls http://0.0.0.0:2379 \
-        --advertise-client-urls http://${IP}:2379 \
-        --listen-peer-urls http://0.0.0.0:2380 \
-        --initial-advertise-peer-urls http://${IP}:2380 \
-        --initial-cluster ${NAME}=http://${IP}:2380 \
-        --data-dir ${BACKUP_DIR} \
+        --data-dir $BACKUP_DIR \
         --force-new-cluster &
     PID=$!
 
