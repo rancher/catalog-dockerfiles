1b81c8c972fd76533a393f01d8dba670876f3085
potzkovge <potzkovge@stud.sbg.ac.at>
2017-02-22 17:21:54 +0100

    Update jenkins_ci.sh
    https://github.com/rancher/catalog-dockerfiles/issues/95

--- jenkins/containers/0.1.1/plugins/jenkins_ci.sh
+++ jenkins/containers/0.1.1/plugins/jenkins_ci.sh
@@ -5,5 +5,5 @@ while [ ! -f /usr/share/jenkins/rancher/plugins.txt ]; do
     sleep 1
 done
 
-/usr/local/bin/plugins.sh /usr/share/jenkins/rancher/plugins.txt
+/usr/local/bin/install-plugins.sh /usr/share/jenkins/rancher/plugins.txt
 exec /bin/tini -- /usr/local/bin/jenkins.sh
