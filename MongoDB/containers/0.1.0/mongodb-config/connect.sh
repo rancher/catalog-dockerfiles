e0a20e65d6b1cd925dd64ba4e685787268cd1614
jri-sp <jeremy.richard@sciences-po.fr>
2017-03-15 18:22:44 +0100

    update rs.initiate to use first host IP instead of hostname

--- MongoDB/containers/0.1.0/mongodb-config/connect.sh
+++ MongoDB/containers/0.1.0/mongodb-config/connect.sh
@@ -9,7 +9,7 @@ GIDDYUP=/opt/rancher/bin/giddyup
 function cluster_init {
 	sleep 10
 	MYIP=$($GIDDYUP ip myip)
-	mongo --eval "printjson(rs.initiate())"
+	mongo --eval 'cfg = { "_id" : "'${REPLSET_NAME}'", "version" : 1, "members" : [ { "_id" : 0, "host" : "'${MYIP}':27017" } ] };rs.initiate(cfg)'
 	for member in $($GIDDYUP ip stringify --delimiter " "); do
 		if [ "$member" != "$MYIP" ]; then
 			mongo --eval "printjson(rs.add('$member:27017'))"
