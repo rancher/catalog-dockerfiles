4dab39a44c742737d1c066d0ce4f2841c9713e66
potzkovge <potzkovge@stud.sbg.ac.at>
2017-05-17 15:59:01 +0200

    updated script to work correctly
    Since install-plugins.sh doesnt take a plugins.txt as an argument one has to use this workaround until the official script is updated. For more on this see this open ticket over at jenkinsci: https://github.com/jenkinsci/docker/issues/348

--- jenkins/containers/0.1.1/plugins/jenkins_ci.sh
+++ jenkins/containers/0.1.1/plugins/jenkins_ci.sh
@@ -5,5 +5,5 @@ while [ ! -f /usr/share/jenkins/rancher/plugins.txt ]; do
     sleep 1
 done
 
-/usr/local/bin/install-plugins.sh /usr/share/jenkins/rancher/plugins.txt
+/usr/local/bin/install-plugins.sh $(cat /usr/share/jenkins/rancher/plugins.txt | tr '\n' ' ')
 exec /bin/tini -- /usr/local/bin/jenkins.sh
